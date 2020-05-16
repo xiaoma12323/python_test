c8d46d1f607ec0d96978e01637134c92c873ad36
OMEN <ma.dingyi@gmail.com>
2020-05-16 22:19:10 +0800

    up

---
+++ com/socket_client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import socket
+import threading
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接
+s.connect(('127.0.0.1', 9999))
+# 接受欢迎信息
+print(s.recv(1024).decode('utf-8'))
+
+for data in [b'Michael', b'Trevor', b'Franklin']:
+    # 发送数据
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
+
+s.send(b'exit')
+s.close()
--- com/test.py
+++ com/test.py
@@ -1,7 +1,2 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import requests
-r = requests.get('http://www.taobao.com')
-print(r.status_code)
-print(r.encoding)
